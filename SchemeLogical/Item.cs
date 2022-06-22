5d225fec4c728489228230af506929972b8e613b
mdryo <mdryomin@bk.ru>
2022-06-22 15:40:48 +0500

    new commit

--- SchemeLogical/Item.cs
+++ SchemeLogical/Item.cs
@@ -15,6 +15,7 @@ namespace SchemeLogical
         public String field_varchar { get; set; }
         public String value_varchar { get; set; }
         public List<Item> operands { get; set; }
+        public int level { get; set; }
         public Item() { }
         public Item(String op, List<Item> items)
         {
