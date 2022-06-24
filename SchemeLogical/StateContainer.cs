be22ee356ace198b96fa1bdfe017a942dc962286
mdryo <mdryomin@bk.ru>
2022-06-24 12:18:01 +0500

    new commit

--- SchemeLogical/StateContainer.cs
+++ SchemeLogical/StateContainer.cs
@@ -3,6 +3,9 @@
     public class StateContainer
     {
         private string? savedString;
+        private string? isOperand;
+        private bool flag=false;
+        private Item current=new();
 
         public string Property
         {
@@ -13,7 +16,32 @@
                 NotifyStateChanged();
             }
         }
+        public string? PropertyOperand
+        {
+            get=>isOperand ?? string.Empty;
+            set
+            {
+                isOperand = value;
+                NotifyStateChanged();
+            }
+        }
+        public bool Flag
+        {
+            get => flag;
+            set 
+            { 
+                flag = value; 
+                NotifyStateChanged(); 
+            }
 
+        }
+        public Item Current
+        {
+            get => current;
+            set { current = value;
+                NotifyStateChanged();
+            }
+        }
         public event Action? OnChange;
 
         private void NotifyStateChanged() => OnChange?.Invoke();
