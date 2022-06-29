b1181b56ba829a6ff0393ea3f6360410713b6825
mdryo <mdryomin@bk.ru>
2022-06-29 16:23:37 +0500

    new commit

---
+++ SchemeLogical/Element.cs
@@ -0,0 +1,12 @@
+﻿namespace SchemeLogical
+{
+    public class Element
+    {
+       public String operandValue { get; set; }
+       public List<Element> operands { get; set; }
+       public String key { get; set; }
+       public String value { get; set; }
+
+
+    }
+}
